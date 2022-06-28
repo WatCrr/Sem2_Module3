adfac7c6f3f2fa550fc252ce3c235696fa139c2a
MatveiVodovoz <mt.vodovoz@gmail.com>
2022-06-28 20:02:25 +0500

    supposedly solved but not

---
+++ task3/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> even_pallys(const std::string& str) {
+  std::vector<int> result (str.length(), 0);
+  int l = 0, r = -1;
+  for (int i = 0; i < str.length(); ++i) {
+    int sim_rad = i > r ? 1 : std::min(result[l + r - i], r - i + 1);
+    while (i + sim_rad < str.length() && i - sim_rad >= 0 && str[i + sim_rad] == str[i - sim_rad]) ++sim_rad;
+    result[i] = sim_rad;
+    if (i + sim_rad - 1 > r)
+      l = i - sim_rad + 1,  r = i + sim_rad - 1;
+  }
+  return result;
+}
+
+std::vector<int> odd_pallys(const std::string& str) {
+  std::vector<int> result (str.length(), 0);
+  int l = 0, r = -1;
+  for (int i = 0; i < str.length(); ++i) {
+    int sim_rad = i > r ? 0 : std::min(result[l + r - i + 1], r - i + 1);
+    while (i + sim_rad < str.length() && i - sim_rad - 1 >= 0 && str[i + sim_rad] == str[i - sim_rad - 1]) ++sim_rad;
+    result[i] = sim_rad;
+    if (i + sim_rad - 1 > r)
+      l = i - sim_rad,  r = i + sim_rad - 1;
+  }
+  return result;
+}
+
+int main() {
+  std::string str;
+  std::getline(std::cin, str);
+
+  int count = 0;
+  std::vector<int> palindromes = even_pallys(str);
+  for (int i = 0; i < palindromes.size(); i++) count += palindromes[i] - 1;
+  palindromes = odd_pallys(str);
+  for (int i = 0; i < palindromes.size(); i++) count += palindromes[i];
+  std::cout << count;
+  return 0;
+}
